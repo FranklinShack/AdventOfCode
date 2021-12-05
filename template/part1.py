359f91e98c895b9f5c3c94ae4ed6ad2d7e31d132
Franklin Shack <franklinshack@gmail.com>
2021-12-05 14:16:07 -0500

    added template folder

---
+++ template/part1.py
@@ -0,0 +1,29 @@
+import sys, getopt
+import re, functools
+from readFile import readFile
+
+def main(argv):
+    L=[]
+    try:
+        opts, args = getopt.getopt(argv, "hps")
+    except getopt.GetoptError:
+        print('usage: part1.py {-p|-s}')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('usage: part1.py {-p|-s}')
+            return 0
+        elif opt == '-p':
+            L = readFile("puzzleInput.txt")
+        elif opt == '-s':
+            L = readFile("sampleInput.txt")
+    print (sol(L))
+    return 0
+
+def sol(L):
+    return 0
+
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
