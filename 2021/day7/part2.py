cc5464437be885b6d6ae25940e54bebd8e155389
Franklin Shack <franklinshack@gmail.com>
2021-12-06 01:19:20 -0500

    created day7 folder

---
+++ 2021/day7/part2.py
@@ -0,0 +1,28 @@
+import sys, getopt
+import re, functools
+from readFile import readFile
+
+def main(argv):
+    L=[]
+    usage=("Usage: "+sys.argv[0]+' {-p -s}')
+    try:
+        opts, args = getopt.getopt(argv, "hps")
+    except getopt.GetoptError:
+        print(usage)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(usage)
+            return 0
+        elif opt == '-p':
+            L = readFile("puzzleInput.txt")
+        elif opt == '-s':
+            L = readFile("sampleInput.txt")
+    print (sol(L))
+    return 0
+
+def sol(L):
+    return 0
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
