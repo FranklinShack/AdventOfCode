e3c5dd4f8f4644d5dec6995e78c02b7ee501946c
Franklin Shack <franklinshack@gmail.com>
2022-12-06 00:12:21 -0500

    day 6 2022

---
+++ 2022/day6/partb.py
@@ -0,0 +1,48 @@
+import sys, getopt
+sys.path.append('../../..')
+import re, functools, itertools, collections, math
+from AdventOfCode.aocUtils import *
+from aocd import submit
+
+def main(argv):
+    L=[]
+    submitFlag = False
+    usage=("Usage: "+sys.argv[0]+' {-p -s -r}')
+    try:
+        opts, args = getopt.getopt(argv, "hpsr")
+    except getopt.GetoptError:
+        print(usage)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(usage)
+            return 0
+        elif opt == '-p':
+            L = readFile("puzzleInput.txt")
+        elif opt == '-s':
+            L = readFile("sampleInput.txt")
+            print(L)
+        elif opt == '-r':
+            submitFlag = True
+    solution=solve(L)
+    print(solution)
+
+    if submitFlag:
+        submit(solution)
+
+    return 0
+
+def solve(L):
+    answer = 14
+    str = L[0]
+
+    for i in list(range(len(str)))[13::]:
+        if len(set(list(str[i-13:i+1])))==14:
+            return answer
+        answer+=1
+
+
+    return answer
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
